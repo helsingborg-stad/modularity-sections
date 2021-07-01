64a1daf71a2ad27bd8d8b59b8d7ae6996fb42cba
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-07-01 16:11:18 +0200

    Remove custom menu rendering, remove margins on modules with util.

--- source/php/App.php
+++ source/php/App.php
@@ -18,9 +18,6 @@ class App
             return $paths;
         });
 
-        //Add sections menu to municipio
-        add_filter('Municipio/Menu/Vertical/Items', array($this, 'renderSectionsMenu'));
-
         //Add classes to mod element
         add_filter('Modularity/Display/BeforeModule', array($this, 'addClass'), 10, 4);
     }
@@ -32,45 +29,10 @@ class App
     public function addClass($markup, $args, $moduleType, $moduleId)
     {
         if (in_array(str_replace("mod-section-", "", $moduleType), array('full', 'featured', 'split'))) {
-            if (function_exists('get_field') && get_field('efx_mod_section_divider', $moduleId)) {
-                $markup = str_replace($moduleType . " ", $moduleType . " has-divider ", $markup);
-            }
+            $markup = str_replace($moduleType . " ", $moduleType . " u-margin--0 ", $markup);
         }
 
         return $markup;
-    }
-
-    /**
-     * Render the sections menu
-     * @return array
-     */
-
-    public function renderSectionsMenu($items)
-    {
-        global $post;
-
-        //Create array if it isen't
-        if (!is_array($items)) {
-            $items = array();
-        }
-
-        //Get modules
-        if ($archiveSlug = \Modularity\Helper\Wp::getArchiveSlug()) {
-            $modules = \Modularity\Editor::getPostModules($archiveSlug);
-        } else {
-            $modules = \Modularity\Editor::getPostModules($post->ID);
-        }
-
-        //Add all modules to array
-        foreach (array('top-sidebar', 'bottom-sidebar') as $sidebar) {
-            if (isset($modules[$sidebar]) && is_array($modules[$sidebar]) && !empty($modules[$sidebar])) {
-                foreach ($modules[$sidebar]['modules'] as $module) {
-                    $items[] = array('title' => $module->post_title, 'link' => '#' . sanitize_title($module->post_title));
-                }
-            }
-        }
-
-        return $items;
     }
 
     /**
