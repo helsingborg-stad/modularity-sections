1fe0e3ea7ba56e4f07cac6ec632a4184427cc275
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2021-07-01 16:08:02 +0200

    Remove uncommented code

--- source/php/Module/Full/views/full.blade.php
+++ source/php/Module/Full/views/full.blade.php
@@ -1,32 +1,3 @@
-{{-- <section id="{{ $sectionID }}" class="section section-full {{ $classes }}" style="background-color: {{ $backgroundColor }}; background-image: url({{ $backgroundImage }}); ">
-  @if(isset($effectOverlayColor) && $effectOverlayColor)
-    <div class="overlay" style="background-color: {{$effectOverlayColor}};"></div>
-  @endif
-  <div class="container {{ $animation['classes'] }}" data-animation="{!! $animation['attribute'] !!}">
-    <div class="grid">
-      <div class="grid-xs-12 section-content">
-        <article class="section-article full">
-          @if (!$hideTitle && !empty($post_title))
-            <h2 class="section-title">{!! apply_filters('the_title', $post_title) !!}</h2>
-          @endif
-          <div class="section-text">
-              {!! $content !!}
-          </div>
-        </article>
-      </div>
-    </div>
-    @if(is_array($submodules) && !empty($submodules))
-      <div class="grid section-submodules">
-          <div class="grid-xs-12">
-          {!! $submoduleRendered !!}
-          </div>
-      </div>
-    @endif
-  </div>
-</section> --}}
-
-
-
 @segment([
     'id'                => $sectionID,
     'title'             => apply_filters('the_title', $post_title),
--- source/php/Module/Split/views/split.blade.php
+++ source/php/Module/Split/views/split.blade.php
@@ -1,28 +1,3 @@
-{{-- <section id="{{ $sectionID }}" class="section section-split background-4 {{ $classes }} {{ $animation['classes'] }}" style="background-color: {{ $backgroundColor }};" data-animation="{!! $animation['attribute'] !!}">
-  <div class="section-image-mobile hidden-md hidden-lg ratio-16-9" style="background-image: url({{ $backgroundImage }}"></div>
-  <div class="container container-fullwidth">
-    <div class="grid">
-      <div class="section-image hidden-xs hidden-sm grid-xs-12 grid-md-5 grid-lg-6" style="background-image: url({{ $backgroundImage }});" data-equal-item></div>
-      <div class="section-content grid-xs-12 grid-md-7 grid-lg-6">
-          <article class="section-article">
-                 <div class="section-text">
-                    @if (!$hideTitle && !empty($post_title))
-                      <h2 class="section-title">{!! apply_filters('the_title', $post_title) !!}</h2>
-                    @endif
-                   {!! $content !!}
-                </div>
-          </article>
-      </div>
-    </div>
-    @if(is_array($submodules) && !empty($submodules))
-      <div class="section-submodules margin-top">
-        {!! $submoduleRendered !!}
-      </div>
-    @endif
-  </div>
-</section>
- --}}
-
 @segment([
     'id'                => $sectionID,
     'title'             => apply_filters('the_title', $post_title),
