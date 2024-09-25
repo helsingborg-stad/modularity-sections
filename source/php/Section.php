6b3bc5dbfd9c853cdc62bcfe4c118edfd95095c4
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-09-25 16:16:34 +0200

    fix: remove comment.

--- source/php/Section.php
+++ source/php/Section.php
@@ -63,14 +63,4 @@ class Section extends \Modularity\Module
       }
       return null;
   }
-
-  /**
-   * Available "magic" methods for modules:
-   * init()            What to do on initialization
-   * data()            Use to send data to view (return array)
-   * style()           Enqueue style only when module is used on page
-   * script            Enqueue script only when module is used on page
-   * adminEnqueue()    Enqueue scripts for the module edit/add page in admin
-   * template()        Return the view template (blade) the module should use when displayed
-   */
 }
